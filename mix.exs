93a0363c0d5e7f8ad3c61b174a280e7bf61388f4
Kate Hedgpeth <katehedgpeth@gmail.com>
2022-09-30 12:55:07 -0400

    install tentacat

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule PrZero.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:tentacat, "~> 2.2.0"}
     ]
   end
 
